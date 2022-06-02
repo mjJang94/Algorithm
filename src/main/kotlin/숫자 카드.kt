b27adf7d3f4b09f564aefd4f470302232fdd42e7
mjJang94 <alswhddl10@naver.com>
2022-06-02 14:39:39 +0900

    듣보잡

--- src/main/kotlin/숫자 카드.kt
+++ src/main/kotlin/숫자 카드.kt
@@ -4,53 +4,67 @@ import java.util.*
 
 /**
  *  5
-    6 3 2 10 -10
-    8
-    10 9 -5 2 3 4 5 -10
+6 3 2 10 -10
+8
+10 9 -5 2 3 4 5 -10
  */
-    fun main() = with(Scanner(System.`in`)) {
+fun main() = with(Scanner(System.`in`)) {
 
-        val builder = StringBuilder()
-        val N = nextInt()
-        val array = IntArray(N)
+    val builder = StringBuilder()
+    val N = nextInt()
+    val array = IntArray(N)
 
-        for (i in 0 until N){
-            array[i] = nextInt()
-        }
+    for (i in 0 until N) {
+        array[i] = nextInt()
+    }
 
-        Arrays.sort(array)
+    Arrays.sort(array)
 
-        val M = nextInt()
+    val M = nextInt()
 
-        for ( i in 0 until M){
-           val checkTargetNumber = nextInt()
-           var left = 0
-           var right = N - 1
+    for (i in 0 until M) {
+        val checkTargetNumber = nextInt()
+        var left = 0
+        var right = N - 1
 
-            while (left <= right){
-                val index = (left + right) / 2
-                val cardNumber = array[index]
+        while (left <= right) {
+            val index = (left + right) / 2
+            val cardNumber = array[index]
 
-                if (cardNumber == checkTargetNumber){
-                    builder.append(1).append(" ")
-                    break
-                }
+            if (cardNumber == checkTargetNumber) {
+                builder.append(1).append(" ")
+                break
+            }
 
-                if (cardNumber > checkTargetNumber){
-                    right = index - 1
-                } else{
-                    left = index + 1
-                }
+            if (cardNumber > checkTargetNumber) {
+                right = index - 1
+            } else {
+                left = index + 1
+            }
 
-                if (left > right){
-                    builder.append(0).append(" ")
-                    break
-                }
+            if (left > right) {
+                builder.append(0).append(" ")
+                break
             }
         }
-
-        println(builder)
     }
+
+    println(builder)
+}
+
+
+fun tt() = with(Scanner(System.`in`)) {
+    val N = nextInt()
+    val colaCount = nextInt() / 2
+    val beerCount = nextInt()
+
+    val totalCount = colaCount + beerCount
+    if (N >= totalCount){
+        println(totalCount)
+    }else{
+        println(N)
+    }
+}
 
 
 
